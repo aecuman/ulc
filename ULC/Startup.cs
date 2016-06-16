6dd27890da0e4825d5a50d996f935b73833bd3a9
Anthony Ecuman <aecuman24@gmail.com>
2016-06-16 19:01:07 +0300

    Add project files.

---
+++ ULC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ULC.Startup))]
+namespace ULC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
